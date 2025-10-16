a846acaa51f15b0a340d2fe50215d45d807ad1b9
Marcos B. Tsuda <tsudaspontodev@gmail.com>
2025-10-15 22:31:59 -0300

    atividade DOM

---
+++ atividade/script.js
@@ -0,0 +1,15 @@
+// let titulo = document.getElementById('titulo');
+
+// if(titulo)
+// {
+// titulo.textContent = 'LISTA DE TAREFAS';
+// }
+
+let titulo = document.getElementById('titulo').innerText = 'LISTA DE TAREFAS';
+
+//const cor = document.querySelector('#sobre p').style.color = "blue";
+
+const cor = document.querySelector('#sobre');
+if(cor){
+    cor.style.color="blue";
+}
